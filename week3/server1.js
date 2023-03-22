c9e8847973d33e625090aab8341a4bdcedb2debf
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-03-22 09:58:06 +0900

    ✨:  노드 서버 만들기

--- week3/server1.js
+++ week3/server1.js
@@ -1,1 +1,20 @@
 // 노드 서버 만들기
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.write("<h1> Hello Node~~</h1>");
+    res.end("<p>Hello Server</p>");
+});
+
+server.listen(8088, ()=>{
+    console.log("8088번 포트에서 서버가 대기 중입니다.");
+});
+
+server.on('listening', (error)=>{
+    console.log("8088번 포트에서 서버가 대기 중입니다.");
+});
+
+server.on('error', (error)=>{
+    console.error(error);
+});
